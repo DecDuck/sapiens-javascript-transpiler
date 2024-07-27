b8e11b89bfe10197b1732454ebb0519a22528278
DecDuck <declanahofmeyr@gmail.com>
2024-07-27 18:00:40 +1000

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "sapiens" {
+declare module "@sapiens/spjs" {
   export type SapiensModule = { [key: string]: Function } & {
     [key: string]: string | number | object | Array<any>;
   };
