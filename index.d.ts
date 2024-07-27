d8dd7e974f93ee9d40461aa1622932ec0f1bc49e
DecDuck <declanahofmeyr@gmail.com>
2024-07-27 18:31:51 +1000

    hopefully this works?

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 import { SapiensModule } from "@sapiens/spjs";
 
 declare module "@sapiens/spjs" {
-  export type SapiensModule = { [key: string]: Function } & {
-    [key: string]: string | number | object | Array<any>;
+  export type SapiensModule ={
+    [key: string]: string | number | object | Array<any> | Function;
   };
 }
 
